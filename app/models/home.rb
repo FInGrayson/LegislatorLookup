862030c59de959deee1a08f1e3063da5f2af226b
Stephen Grayson <fingrayson@gmail.com>
2017-02-17 21:08:54 -0500

    add more stuff

--- app/models/home.rb
+++ app/models/home.rb
@@ -4,6 +4,10 @@ class Home < ApplicationRecord
     legislators = HTTParty.get("https://congress.api.sunlightfoundation.com/legislators/locate?zip=#{zip}")
   end
 
+  def self.bills(id)
+    bills = HTTParty.get("https://congress.api.sunlightfoundation.com/bills?sponsor_id__in=#{id}")
+  end
+
   def self.chamber(chamber)
     if chamber == "house"
       "House of Representatives"
